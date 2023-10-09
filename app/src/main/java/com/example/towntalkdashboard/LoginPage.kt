d165368185f4e0725136de870f20b69a91dc40a7
Ryan <ryan.pulocan6@gmail.com>
2023-10-09 14:02:28 +0800

    connected login to main

--- app/src/main/java/com/example/towntalkdashboard/LoginPage.kt
+++ app/src/main/java/com/example/towntalkdashboard/LoginPage.kt
@@ -1,11 +1,26 @@
 package com.example.towntalkdashboard
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class LoginPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_page)
+
+        val btnlogin = findViewById<Button>(R.id.button)
+        btnlogin.setOnClickListener {
+            val intent = Intent (this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        val btnsignup = findViewById<TextView>(R.id.signup)
+        btnsignup.setOnClickListener{
+            val intent = Intent (this,SignupPage::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/towntalkdashboard/SignupPage.kt
+++ app/src/main/java/com/example/towntalkdashboard/SignupPage.kt
@@ -1,11 +1,25 @@
 package com.example.towntalkdashboard
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SignupPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup_page)
+
+        val btnbacktologin = findViewById<Button>(R.id.button2)
+        btnbacktologin.setOnClickListener {
+            val intent = Intent (this, LoginPage::class.java)
+            startActivity(intent)
+        }
+
+        val btnsigup = findViewById<Button>(R.id.submit)
+        btnsigup.setOnClickListener {
+            val intent = Intent (this, LoginPage::class.java)
+            startActivity(intent)
+        }
     }
 }
