e41bbb3bff01a2ea90c61599f0e3a32129671e05
Ryan <ryan.pulocan6@gmail.com>
2023-10-09 12:24:40 +0800

    added bottom navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,7 +25,13 @@ android {
                 "proguard-rules.pro"
             )
         }
+
     }
+
+    buildFeatures{
+        viewBinding = true
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
--- app/src/main/java/com/example/towntalkdashboard/MainActivity.kt
+++ app/src/main/java/com/example/towntalkdashboard/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.example.towntalkdashboard
 
+import androidx.fragment.app.Fragment
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.towntalkdashboard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(HomeFragment())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+
+            when(it.itemId){
+                R.id.navigation_home -> replaceFragment(HomeFragment())
+                R.id.navigation_report -> replaceFragment(ReportFragment())
+                R.id.navigation_profile -> replaceFragment(ProfileFragment())
+
+                else ->{
+
+                }
+            }
+                true
+        }
+    }
+
+    private fun replaceFragment(fragment : Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
     }
 }
