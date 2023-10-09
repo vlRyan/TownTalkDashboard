97c183c2a5fcf4e313b0364d7859dfcb0302164d
Ryan <ryan.pulocan6@gmail.com>
2023-10-09 13:38:52 +0800

    continue

---
+++ app/src/main/java/com/example/towntalkdashboard/MyReportPage.kt
@@ -0,0 +1,11 @@
+package com.example.towntalkdashboard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyReportPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_report_page)
+    }
+}
