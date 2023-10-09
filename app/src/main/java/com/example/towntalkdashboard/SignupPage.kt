eab36f76921aadf8a340cc1961559bb53db2525c
Ryan <ryan.pulocan6@gmail.com>
2023-10-09 10:42:10 +0800

    added project

---
+++ app/src/main/java/com/example/towntalkdashboard/SignupPage.kt
@@ -0,0 +1,11 @@
+package com.example.towntalkdashboard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignupPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup_page)
+    }
+}
