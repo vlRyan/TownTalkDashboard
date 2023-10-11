e8cfccda0ff474c5b7a2dbbf03198408d0e2c88c
Ryan <ryan.pulocan6@gmail.com>
2023-10-11 11:58:42 +0800

    improved ui

--- app/src/main/java/com/example/towntalkdashboard/LoginPage.kt
+++ app/src/main/java/com/example/towntalkdashboard/LoginPage.kt
@@ -5,22 +5,39 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.core.text.HtmlCompat
+import android.text.SpannableStringBuilder
+import android.text.style.ForegroundColorSpan
+import android.graphics.Color
+import android.text.SpannableString
+import android.text.Spannable
+
 
 class LoginPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_page)
 
-        val btnlogin = findViewById<Button>(R.id.button)
-        btnlogin.setOnClickListener {
+        val combinedText = findViewById<TextView>(R.id.app_title)
+        val text = SpannableString("TownTalk")
+        text.setSpan(ForegroundColorSpan(Color.WHITE), 0, 4, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        text.setSpan(ForegroundColorSpan(Color.RED), 4, 8, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        combinedText.text = text
+
+        val loginButton = findViewById<Button>(R.id.login_btn)
+        loginButton.setOnClickListener {
             val intent = Intent (this, MainActivity::class.java)
             startActivity(intent)
         }
 
-        val btnsignup = findViewById<TextView>(R.id.signup)
-        btnsignup.setOnClickListener{
+        val signupButton = findViewById<TextView>(R.id.signup_btn)
+        signupButton.setOnClickListener{
             val intent = Intent (this,SignupPage::class.java)
             startActivity(intent)
         }
+
+
+
+
     }
 }
--- app/src/main/java/com/example/towntalkdashboard/SignupPage.kt
+++ app/src/main/java/com/example/towntalkdashboard/SignupPage.kt
@@ -10,16 +10,12 @@ class SignupPage : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup_page)
 
-        val btnbacktologin = findViewById<Button>(R.id.button2)
-        btnbacktologin.setOnClickListener {
+        val backLogin = findViewById<Button>(R.id.back_btn)
+        backLogin.setOnClickListener {
             val intent = Intent (this, LoginPage::class.java)
             startActivity(intent)
         }
 
-        val btnsigup = findViewById<Button>(R.id.submit)
-        btnsigup.setOnClickListener {
-            val intent = Intent (this, LoginPage::class.java)
-            startActivity(intent)
-        }
+
     }
 }
