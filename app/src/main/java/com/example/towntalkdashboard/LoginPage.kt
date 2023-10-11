7a35ce2e7cbbfbeededd7c97a9b372d861fe2dbb
Ryan <ryan.pulocan6@gmail.com>
2023-10-11 21:48:20 +0800

    connected firebase but cant login

--- app/src/main/java/com/example/towntalkdashboard/LoginPage.kt
+++ app/src/main/java/com/example/towntalkdashboard/LoginPage.kt
@@ -3,41 +3,64 @@ package com.example.towntalkdashboard
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.widget.TextView
-import androidx.core.text.HtmlCompat
-import android.text.SpannableStringBuilder
 import android.text.style.ForegroundColorSpan
 import android.graphics.Color
 import android.text.SpannableString
 import android.text.Spannable
+import android.widget.Toast
+import com.example.towntalkdashboard.databinding.ActivityLoginPageBinding
+import com.google.firebase.auth.FirebaseAuth
 
 
 class LoginPage : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginPageBinding
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login_page)
+        binding = ActivityLoginPageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val combinedText = findViewById<TextView>(R.id.app_title)
+        val combinedText = findViewById<TextView>(R.id.appTitle)
         val text = SpannableString("TownTalk")
         text.setSpan(ForegroundColorSpan(Color.WHITE), 0, 4, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
         text.setSpan(ForegroundColorSpan(Color.RED), 4, 8, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
         combinedText.text = text
 
-        val loginButton = findViewById<Button>(R.id.login_btn)
-        loginButton.setOnClickListener {
-            val intent = Intent (this, MainActivity::class.java)
-            startActivity(intent)
-        }
+        firebaseAuth = FirebaseAuth.getInstance()
 
-        val signupButton = findViewById<TextView>(R.id.signup_btn)
+        val signupButton = findViewById<TextView>(R.id.signupBtn)
         signupButton.setOnClickListener{
             val intent = Intent (this,SignupPage::class.java)
             startActivity(intent)
         }
 
+        binding.loginBtn.setOnClickListener{
+            val email = binding.emailInput.text.toString()
+            val password = binding.passwordInput.text.toString()
 
+            if (email.isNotEmpty() && password.isNotEmpty()){
+                firebaseAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener{
+                    if(it.isSuccessful){
+                        val intent = Intent(this,MainActivity::class.java)
+                        startActivity(intent)
+                    }else{
+                        Toast.makeText(this,it.exception.toString(), Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }else{
+                Toast.makeText(this,"Empty", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
 
+    override fun onStart() {
+        super.onStart()
 
+        if(firebaseAuth.currentUser != null){
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/towntalkdashboard/SignupPage.kt
+++ app/src/main/java/com/example/towntalkdashboard/SignupPage.kt
@@ -4,18 +4,49 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
+import com.example.towntalkdashboard.databinding.ActivitySignupPageBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class SignupPage : AppCompatActivity() {
+
+    private lateinit var binding:ActivitySignupPageBinding
+    private lateinit var firebaseAuth:FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signup_page)
 
-        val backLogin = findViewById<Button>(R.id.back_btn)
-        backLogin.setOnClickListener {
+        binding = ActivitySignupPageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        val backButton = findViewById<Button>(R.id.backBtn)
+        backButton.setOnClickListener {
             val intent = Intent (this, LoginPage::class.java)
             startActivity(intent)
         }
 
+        binding.signupBtn.setOnClickListener{
+            val email = binding.emailInput.text.toString()
+            val password = binding.passwordInput.text.toString()
+            val confirmPassword = binding.confirmPasswordInput.text.toString()
 
+            if (email.isNotEmpty() && password.isNotEmpty() && confirmPassword.isNotEmpty()){
+                if (password == confirmPassword){
+                    firebaseAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener{
+                        if(it.isSuccessful){
+                            val intent = Intent(this,LoginPage::class.java)
+                            startActivity(intent)
+                        }else{
+                            Toast.makeText(this,it.exception.toString(),Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }else{
+                    Toast.makeText(this,"Password do not match", Toast.LENGTH_SHORT).show()
+                }
+            }else{
+                Toast.makeText(this,"Empty Field",Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
